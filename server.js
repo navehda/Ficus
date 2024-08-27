bd8fe51554531e004633fdfe05d8477ee1002174
Naveh <navehd@cymulate.com>
2024-08-27 18:15:18 +0300

    Modified all HTML webpages to correct paths

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
